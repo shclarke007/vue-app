6608d79912d5b8dcfadd55244029bebb4ef05366
Shaneil Clarke <shaneil.clarke@worldwebdevelopment.co.uk>
2019-06-11 16:53:02 +0100

    initial Vue create app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="I am in Vue.js App"/>
+    <EmployeeTable />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from './components/EmployeeTable.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    EmployeeTable
   }
 }
 </script>
@@ -24,5 +27,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
 }
 </style>
