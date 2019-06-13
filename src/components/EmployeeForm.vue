821662bd36d860ed19faf728459552a13e916dfb
Shaneil Clarke <shaneil.clarke@worldwebdevelopment.co.uk>
2019-06-13 10:12:15 +0100

    added feedback message when form is successfully submitted or has an error

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -2,9 +2,31 @@
   <div class="form-container">
     <form id="employee-form" @submit.prevent="handleSubmit">
       <label>Employee name</label>
-      <input v-model="employee.name" for="employee-name" type="text" placeholder="enter name"/>
+      <input 
+        v-model="employee.name"
+        :class="{ 'has-error': submitting && invalidName }"
+        @focus="clearStatus" 
+        @keypress="clearStatus"
+        for="employee-name" 
+        type="text" 
+        placeholder="enter name"
+        ref="first"
+      />
       <label>Employee email</label>
-      <input v-model="employee.email" for="employee-email" type="text" placeholder="enter email"/>
+      <input 
+        v-model="employee.email"
+        :class=" { 'has-error' : submitting && invalidEmail }"
+        @focus="clearStatus" 
+        for="employee-email" 
+        type="text" 
+        placeholder="enter email"
+      />
+      <p v-if="error && submitting" class="error-message">
+        ❗ Please fill out all required fields
+      </p>
+      <p v-if="success" class="success-message">
+        ✅ Employee successfully added
+      </p>
       <button>Add Employee</button>
     </form>
   </div>
@@ -28,12 +50,13 @@ export default {
     handleSubmit() {
       this.submitting = true
       this.clearStatus()
-      
+
       if(this.invalidName || this.invalidEmail) {
         this.error = true
         return
       }
       this.$emit('add:employee', this.employee)
+      this.$refs.first.focus()
       this.employee == {
         name: '',
         email: '',
@@ -66,6 +89,18 @@ export default {
   .form-container {
     padding: 20px;
     
+  }
+
+  [class*='-message'] {
+    font-weight: 500;
+  }
+
+  .error-message {
+    color: #d33c40;
+  }
+
+  .success-message {
+    color: #32a95d;
   }
 
   @media screen and (min-width: 600px) {
